190b32970c0d644649de8914d7a253301efe3919
Carlos Alberto de Lima Campos <carlosalbertode.campos@gmail.com>
2022-02-28 13:34:37 -0300

    Front-End Table

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
   public eventos: any = [];
-  public widthImg: number = 120;
-  public marginImg: number = 1.5;
-  public isHidden: boolean = true;
+  widthImg: number = 120;
+  marginImg: number = 1.5;
+  isHidden: boolean = false;
+  private _filtroLista: string = '';
 
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventos = this.filtroLista
+      ? this.filtrarPor(this.filtroLista)
+      : this.getEventos();
+  }
+
+  filtrarPor(filtro: string): any {
+    filtro = filtro.toLocaleLowerCase();
+
+    return this.eventos.filter(
+      (evento: any) =>
+        evento.tema.toLocaleLowerCase().indexOf(filtro) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtro) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -20,9 +41,9 @@ export class EventosComponent implements OnInit {
   }
 
   public getEventos(): void {
-    this.http.get("https://localhost:5001/api/eventos").subscribe(
-      response => this.eventos = response,
-      error => console.log(error)
+    this.http.get('https://localhost:5001/api/eventos').subscribe(
+      (response) => (this.eventos = response),
+      (error) => console.log(error)
     );
   }
 }
